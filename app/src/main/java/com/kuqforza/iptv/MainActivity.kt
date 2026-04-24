93f9834d8d535459fb92d58c1d1d0e7540a85bd2
yangtresso-del <yangtresso-del@users.noreply.github.com>
2026-04-24 13:30:45 +0200

    fix duplicate import

--- app/src/main/java/com/kuqforza/iptv/MainActivity.kt
+++ app/src/main/java/com/kuqforza/iptv/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.app.PictureInPictureParams
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.content.Intent
 import com.kuqforza.iptv.web.WebAdminService
 import android.os.Build
 import android.os.StrictMode
